85ff5cbbefe00c09de23ea10590cdf806cbf1223
davidmartinez9 <davidloso87@gmail.com>
2019-06-24 18:47:39 -0500

    git init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
+class App extends Component {
+  
+ 
+  
+  render() {
+   
+  
+    return (
     <div className="App">
-      <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <div className="row">
+        <div style={{ height: "75vh"}} className="container valign-wrapper">
+        <p className="col s12 center-align">
+        Somos un taller de hojalatería y pintura móvil, venimos a ti. 
+        Nuestros técnicos son altamente capacitados y experimentados reparan tu vehículo en sus instalaciones. 
+        Ya sea que esté en su casa o en el trabajo, todavía se beneficiará de un ahorro masivo de al menos el 60% en comparación
+         con los precios tradicionales de los talleres. El costo de la reparación de esos desgastes y arañazos, etc., es casi siempre más 
+         barato que el exceso de su seguro, por lo que no es necesario un reclamo de seguro y mantiene intacto su bono vital sin reclamos.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        </div>
+      </div>
     </div>
   );
 }
+}
+
 
 export default App;
